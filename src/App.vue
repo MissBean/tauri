2e74f8c05a3f74f765a87681080d4e7128226055
窦雷敏 <douleimin@xuetangx.com>
2024-02-29 18:10:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+import Menu from "./components/Menu.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Menu />
+
+    <Greet />
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="menu">
+    <ul class="menu-list">
+      <li v-for="item in menuItems" :key="item.name">
+        <router-link :to="item.path">{{ item.name }}</router-link></li>
+    </ul>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const menuItems = ref([
+  { name: "上传区", path: "/upload" },
+  { name: "相册", path: "/piclist" },
+  { name: "图床设置", path: "/setting" },
+]);
+</script>
+<style scoped>
+  .menu {
+    width: 150px;
+    height: 100vh;
+    background: #d4d4d470;
+    text-align: left;
+  }
+  .menu-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .menu-list li {
+    padding: 10px 15px;
+  }
+  .menu-list li a {
+    display: block;
+    text-decoration: none;
+    color: #333;
+  }
+</style>
+
+
