859854c44546b4c884779dba2814786959fa3480
silence <douleimin@xuetangx.com>
2024-04-02 15:06:06 +0800

    icon

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
-import Menu from "./components/Menu.vue";
-</script>
-
 <template>
-  <div class="container">
-    <Menu />
-
-    <Greet />
+  <div>
+    <input type="file" @change="uploadFile" />
   </div>
 </template>
 
-<style scoped>
-  
-</style>
+<script setup>
+import OSS from 'ali-oss';
+
+const uploadFile = async function(event) {
+  const client = new OSS({
+    region: '<Your OSS region>',
+    accessKeyId: '<Your AccessKey ID>',
+    accessKeySecret: '<Your AccessKey Secret>',
+    bucket: '<Your Bucket name>',
+  });
+
+  try {
+    const file = event.target.files[0];
+    const result = await client.put('<Your file path in OSS>', file);
+    console.log('File uploaded successfully:', result);
+  } catch (error) {
+    console.error('Error uploading file:', error);
+  }
+}
+
+</script>
